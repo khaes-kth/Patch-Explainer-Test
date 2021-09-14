6c46a523c554d5a1ae937d334d6370be7a8cb9f6
Khashayar Etemadi <khaes@kth.se>
2021-09-14 17:30:02 +0200

    if condition incorrected

--- src/main/java/NumberAnalyzer.java
+++ src/main/java/NumberAnalyzer.java
@@ -1,6 +1,6 @@
 public class NumberAnalyzer {
     public static String analyze(int x){
-        if (x < 0)
+        if (x > 0)
             return x + " is smaller than zero.";
         else if (x == 0)
             return x + " is exactly zero.";
